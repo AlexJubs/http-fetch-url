32b36a4008488fe6149909431c00f58d4e20ebb8
Alexander Jabbour <ajabbour@uwaterloo.ca>
2020-10-29 03:28:24 -0400

    parsing works well now

--- url_cli.c
+++ url_cli.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define help_message "Usage of ./url_cli: \n --profile <int> \n		A positive integer, number of http requests (default 1) \n --url <url> \n 		URL to fetch data from, as a string (default `https://alex-worker.alexjubs.workers.dev/`)"
 
 void fetch_url(char * url);
 
 int main( int argc, char** argv ) {
+	// the default values
 	char * url = "https://alex-worker.alexjubs.workers.dev/";
 	int profile = 1;
 
@@ -36,7 +38,7 @@ int main( int argc, char** argv ) {
 			}
 			else if (strcmp(argv[3], "--profile") == 0) {
 				// store the url in the url char
-				profile = *strdup(argv[4]) - '0';
+				profile = atoi(strdup(argv[4]));
 			}
 		}
 	}
